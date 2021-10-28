c90b13c9813e01763422bc31390e5701eb0976a4
Tony <54847558+Kybernetique@users.noreply.github.com>
2021-10-28 16:40:03 +0400

    Начало работы с Java

---
+++ src/Direction.java
@@ -0,0 +1,8 @@
+// Перечисления для задания направления движения для кнопок
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    // Точка входа в программу
+    public static void main(String[] args)
+    {
+        // Создание формы
+        BoatGUI form = new BoatGUI();
+    }
+}
